865e37dc6ca42a3154ef01ce90987720530ae933
DareFox <47672780+DareFox@users.noreply.github.com>
2024-04-17 17:52:50 +0300

    tooling: create function to split list evenly

--- src/main/kotlin/ListExtension.kt
+++ src/main/kotlin/ListExtension.kt
@@ -1,3 +1,34 @@
+package me.darefox
+
+/**
+ * Splits the elements of the array into N equal parts as evenly as possible.
+ *
+ * If the number of elements (N) is less than the specified number of parts (X),
+ * it returns a nested array of N-sized lists, each containing one element.
+ *
+ * @param parts The number of parts to split the array into.
+ * @return A list of lists containing the split elements.
+ * @throws IllegalArgumentException if `parts` is less than or equal to zero.
+ */
+fun <T> List<T>.splitIntoParts(parts: Int): List<List<T>> {
+    require(parts > 0) {
+        "Parts should be greater than zero (was $parts)"
+    }
+
+    val partsList = buildList<MutableList<T>> {
+        repeat(parts) {
+            add(mutableListOf())
+        }
+    }
+
+    for ((index, value) in this.withIndex()) {
+        val partIndex = index % parts
+        partsList[partIndex].add(value)
+    }
+
+    return partsList.filter { it.isNotEmpty() }
+}
+
 /**
  * Remove empty lines at the end of list
  */
