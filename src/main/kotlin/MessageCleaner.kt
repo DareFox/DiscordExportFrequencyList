e75e84a66b7a10819c5978c2ee542137d4eb5259
DareFox <47672780+DareFox@users.noreply.github.com>
2024-04-17 17:53:17 +0300

    feat: Add MessageCleaner that cleans messages in parallel

---
+++ src/main/kotlin/MessageCleaner.kt
@@ -0,0 +1,60 @@
+package me.darefox
+
+import kotlinx.coroutines.*
+
+data class CleanMessage(
+    val header: String,
+    val cleanLines: List<String>,
+)
+
+object MessageCleaner {
+    val scope = CoroutineScope(Dispatchers.Default + CoroutineName("MessageCleanerScope"))
+    /**
+     * Remove embeds, attachments, urls in messages leaving only text words
+     */
+    suspend fun clean(messages: List<Message>): List<CleanMessage> {
+        val split = messages.splitIntoParts(RuntimeInfo.cpuThreads)
+
+        val asyncClean = split.map { list ->
+            scope.async {
+                val cleanList = mutableListOf<CleanMessage>()
+                for (message in list) {
+                    val cleanedLines =
+                        message.removeURLs().removeAttachmentAndEmbed()
+                            .lines.trimEmptyEndLines()
+
+                    if (cleanedLines.isNotEmpty()) {
+                        cleanList += CleanMessage(message.header, cleanedLines)
+                    }
+                }
+                cleanList
+            }
+        }
+
+        return asyncClean.awaitAll().flatten()
+    }
+
+    private fun Message.removeURLs(): Message {
+        val urlLikeRegex = "[-a-zA-Z0-9@:%_\\+.~#?&//=]{2,256}\\.[a-z]{2,4}\\b(\\/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)?".toRegex()
+        return Message(header, lines.map {
+            it.replace(urlLikeRegex, "")
+        })
+    }
+
+    private fun Message.removeAttachmentAndEmbed(): Message {
+        val newLines = mutableListOf<String>()
+        val discordTags = listOf(
+            "{Stickers}",
+            "{Embed}",
+            "{Attachments}"
+        )
+        for (line in lines) {
+            if (line in discordTags) {
+                break
+            } else {
+                newLines += line
+            }
+        }
+        return Message(header, newLines)
+    }
+}
