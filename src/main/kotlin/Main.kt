dafc20748d90e8d50dc8e4a4a3acb1876a06f788
DareFox <47672780+DareFox@users.noreply.github.com>
2024-04-17 18:09:19 +0300

    feat: use arguments for selecting input/output file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,29 @@
 package me.darefox
 
-fun main() {
+import java.io.File
 
+suspend fun main(args: Array<String>) {
+    val argsAsList = args.toList()
+    if (argsAsList.isEmpty()) {
+        println("Arguments: [inputfile.txt] [outputfile (optional)]")
+        return
+    }
+
+    val inputFile = File(argsAsList[0])
+
+    val outputArg = argsAsList.getOrNull(1)
+    val outputFile = if (outputArg == null) {
+        createNeighborFile(inputFile)
+    } else {
+        File(outputArg)
+    }
+
+    FrequencyFileGenerator.generate(inputFile, outputFile)
+}
+
+private fun createNeighborFile(file: File): File {
+    val fileName = file.nameWithoutExtension + "-freq-list"
+    val parentDir = file.parentFile
+    val newFileName = "$fileName.txt"
+    return File(parentDir, newFileName)
 }
